d17d1ddcd1e786fdb59bb3b99f8ca11159ded807
Liyang-Zhang <1219187848@qq.com>
2024-05-29 16:24:56 +0800

    Fix: disable sentry

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,5 +1,4 @@
 # ruff: noqa: E501
-import logging
 
 from .base import *  # noqa: F403
 from .base import DATABASES
@@ -131,21 +130,6 @@ LOGGING = {
 
 # Sentry
 # ------------------------------------------------------------------------------
-SENTRY_DSN = env("SENTRY_DSN")
-SENTRY_LOG_LEVEL = env.int("DJANGO_SENTRY_LOG_LEVEL", logging.INFO)
-
-sentry_logging = LoggingIntegration(  # noqa: F405
-    level=SENTRY_LOG_LEVEL,  # Capture info and above as breadcrumbs
-    event_level=logging.ERROR,  # Send errors as events
-)
-integrations = [sentry_logging, DjangoIntegration(), RedisIntegration()]  # noqa: F405
-sentry_sdk.init(  # noqa: F405
-    dsn=SENTRY_DSN,
-    integrations=integrations,
-    environment=env("SENTRY_ENVIRONMENT", default="production"),
-    traces_sample_rate=env.float("SENTRY_TRACES_SAMPLE_RATE", default=0.0),
-)
-
 
 # Your stuff...
 # ------------------------------------------------------------------------------
